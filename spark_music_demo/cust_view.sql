6fa3d033801b90800b74d0d1d80dd17598cba07c
Ubuntu <ubuntu@ip-172-31-35-235.us-west-2.compute.internal>
2015-09-17 20:13:47 +0000

    fixup syntax

--- spark_music_demo/cust_view.sql
+++ spark_music_demo/cust_view.sql
@@ -1,9 +1,9 @@
 SELECT
-CAST(`row_key` AS INTEGER) AS `custid`
-CAST(`t`.`cdata`['name'] AS VARCHAR(20)) AS `name`
-CAST(`t`.`cdata`['campaign'] AS INTEGER) AS `campaign`
-CAST(`t`.`cdata`['gender'] AS INTEGER) AS `gender`
-CAST(`t`.`cdata`['level'] AS INTEGER) AS `level`
-CAST(`t`.`cdata`['address'] AS VARCHAR(40)) AS `address`
+CAST(`row_key` AS INTEGER) AS `custid`,
+CAST(`t`.`cdata`['name'] AS VARCHAR(20)) AS `name`,
+CAST(`t`.`cdata`['campaign'] AS INTEGER) AS `campaign`,
+CAST(`t`.`cdata`['gender'] AS INTEGER) AS `gender`,
+CAST(`t`.`cdata`['level'] AS INTEGER) AS `level`,
+CAST(`t`.`cdata`['address'] AS VARCHAR(40)) AS `address`,
 CAST(`t`.`cdata`['zip'] AS VARCHAR(5)) AS `zip`
-FROM `dfs`.`default`.`./user/mapr/cust_table` AS `t`"
+FROM `dfs`.`default`.`./user/mapr/cust_table` AS `t`
--- spark_music_demo/live_view.sql
+++ spark_music_demo/live_view.sql
@@ -1,8 +1,8 @@
 SELECT
-CAST(`row_key` AS INTEGER) AS `custid`
+CAST(`row_key` AS INTEGER) AS `custid`,
 CAST(`t`.`ldata`['morn_tracks'] AS FLOAT) AS `avg_morn`,
-CAST(`t`.`ldata`['aft_tracks'] AS FLOAT) AS `avg_aft`
+CAST(`t`.`ldata`['aft_tracks'] AS FLOAT) AS `avg_aft`,
 CAST(`t`.`ldata`['eve_tracks'] AS FLOAT) AS `avg_eve`,
 CAST(`t`.`ldata`['night_tracks'] AS FLOAT) AS `avg_night`, CAST(`t`.`ldata`['unique_tracks'] AS FLOAT) AS `avg_unique`,
 CAST(`t`.`ldata`['mobile_tracks'] AS FLOAT) AS `avg_mobile`
-FROM `dfs`.`default`.`./user/mapr/live_table` AS `t`"
+FROM `dfs`.`default`.`./user/mapr/live_table` AS `t`
